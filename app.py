b3d0ee5aa4ff25477c1b44501e694f16522148b5
Caíque Gonçalves <caiquegoncalves83@gmail.com>
2021-12-09 17:46:46 -0300

    Criação de templates (layout, add, addpost)

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from typing import Text
+from flask import Flask, render_template, url_for, request, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+about_text = 'Testando'
 
 @app.route('/')
 @app.route('/index')
@@ -22,6 +39,33 @@ def post():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    return render_template('account.html', text = about_text)
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+    return redirect(url_for('index'))
+
+    db.session.add(post)
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
